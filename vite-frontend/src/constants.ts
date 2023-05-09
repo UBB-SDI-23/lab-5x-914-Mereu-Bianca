d554581255e03d69979efeb8c3be342113ae57b9
914-Mereu-Bianca <biamereu2002@gmail.com>
2023-05-09 16:46:23 +0300

    add changes

--- vite-frontend/src/constants.ts
+++ vite-frontend/src/constants.ts
@@ -1,7 +1,7 @@
 const PROD_BACKEND_API_URL = "/api";
 // const DEV_BACKEND_API_URL = "http://ec2-16-171-35-8.eu-north-1.compute.amazonaws.com/api";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
-// const DEV_BACKEND_API_URL = "https://sdi-biamereu.mooo.com/api";
+// const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+const DEV_BACKEND_API_URL = "https://sdi-biamereu.mooo.com/api";
 
 export const BACKEND_API_URL =
 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
